7cd74d2127383a9b4756bba79808be8885e33018
Ezan777 <ezangri@gmail.com>
2022-04-04 15:53:33 +0200

    restored all the quotes

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const quotes = [
-    /* 'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
+    'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
     'There is nothing more deceptive than an obvious fact.',
     'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
     'I never make exceptions. An exception disproves the rule.',
     'What one man can invent another can discover.',
     'Nothing clears up a case so much as stating it to another person.',
     'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
-    'London, that great cesspool into which all the loungers and idlers of the Empire are irresistibly drained.', */
-    'oro giallo',
+    'London, that great cesspool into which all the loungers and idlers of the Empire are irresistibly drained.',
 ];
 // Variables needed to store the list of words and the index of the word the player is currently typing
 let words = [];
@@ -20,7 +19,6 @@ const messageElement = document.getElementById('message');
 const typedValueElement = document.getElementById('typed-value');
 
 function onInput() {
-    console.log(1);
     // Get the current word
     const currentWord = words[wordIndex];
     // Get the current value
